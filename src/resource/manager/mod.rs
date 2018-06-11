37f3d387cd7ffcd112908577265166d00b3d98eb
Imota Eru <threnody96@gmail.com>
2018-06-11 23:12:17 +0900

    master

--- src/resource/manager/mod.rs
+++ src/resource/manager/mod.rs
@@ -3,44 +3,29 @@ extern crate sdl2;
 use super::storage::Storage;
 use std::collections::HashMap;
 use std::rc::Rc;
-use std::cell::RefCell;
-use self::texture::TextureManager;
-use self::plaindata::PlainDataManager;
-use self::sdl2::render::Texture;
 
 pub trait ResourceLoader<'l> {
     type Item;
     fn resource_name(&'l self) -> String;
     fn load_resource(&'l self, storage: Rc<Box<Storage>>, path: &str) -> Result<Self::Item, String>;
-
 }
 
 pub struct ResourceManager<'l, R> where R: 'l + ResourceLoader<'l> {
     cache: HashMap<String, Rc<R::Item>>,
     loader: &'l R,
-    storages: HashMap<String, Rc<Box<Storage>>>
 }
 
 impl<'l, R> ResourceManager<'l, R> where R: 'l + ResourceLoader<'l> {
 
-    pub fn new(storages: Vec<Box<Storage>>, loader: &'l R) -> Self {
-        Self { cache: HashMap::new(), loader: loader, storages: Self::convert_to_storage_map(storages) }
+    pub fn new(loader: &'l R) -> Self {
+        Self { cache: HashMap::new(), loader: loader }
     }
 
-    fn convert_to_storage_map(storages: Vec<Box<Storage>>) -> HashMap<String, Rc<Box<Storage>>> {
-        let mut storage_map: HashMap<String, Rc<Box<Storage>>>  = HashMap::new();
-        for storage in storages {
-            storage_map.insert(storage.name(), Rc::new(storage));
-        }
-        storage_map
-    }
-
-    pub fn load(&mut self, storage_name: &str, path: &str) -> Result<Rc<R::Item>, String> {
-        let cache_key = Self::generate_cache_key(storage_name, path);
+    pub fn load(&mut self, storage: Rc<Box<Storage>>, path: &str) -> Result<Rc<R::Item>, String> {
+        let cache_key = Self::generate_cache_key(&storage.name(), path);
         let data = self.cache.get(&cache_key).cloned();
         match data {
             None => {
-                let storage = try!(self.storages.get(storage_name).ok_or(format!("storage not found: {}", storage_name))).clone();
                 let resource = Rc::new(try!(self.loader.load_resource(storage, path)));
                 self.cache.insert(cache_key, resource.clone());
                 Ok(resource)
@@ -49,37 +34,8 @@ impl<'l, R> ResourceManager<'l, R> where R: 'l + ResourceLoader<'l> {
         }
     }
 
-    pub fn list(&self, storage_name: &str, dir: Option<&str>) -> Result<Vec<String>, String> {
-        self.storages.get(storage_name).unwrap().list(dir)
-    }
-
-    pub fn save(&self, storage_name: &str, path: &str, data: &Vec<u8>) -> Result<(), String> {
-        self.storages.get(storage_name).unwrap().save(path, data)
-    }
-
     fn generate_cache_key(storage_name: &str, path: &str) -> String {
         format!("{}/{}", storage_name, path)
-    }
-
-}
-
-pub struct Resources<'l, W> where W: 'l {
-    texture: RefCell<TextureManager<'l, W>>,
-    plaindata: RefCell<PlainDataManager<'l>>
-}
-
-impl<'l, W> Resources<'l, W> {
-
-    pub fn new(texture: TextureManager<'l, W>, plaindata: PlainDataManager<'l>) -> Self {
-        Self { texture: RefCell::new(texture), plaindata: RefCell::new(plaindata) }
-    }
-
-    pub fn load_plaindata(&self, storage_name: &str, path: &str) -> Result<Rc<Vec<u8>>, String> {
-        self.plaindata.borrow_mut().load(storage_name, path)
-    }
-
-    pub fn load_texture(&self, storage_name: &str, path: &str) -> Result<Rc<Texture<'l>>, String> {
-        self.texture.borrow_mut().load(storage_name, path)
     }
 
 }
--- src/resource/mod.rs
+++ src/resource/mod.rs
@@ -1,2 +1,60 @@
+extern crate sdl2;
+
+use std::cell::RefCell;
+use std::collections::HashMap;
+use std::rc::Rc;
+use self::storage::Storage;
+use self::manager::texture::TextureManager;
+use self::manager::plaindata::{ PlainDataLoader, PlainDataManager };
+use self::sdl2::render::{ Texture, TextureCreator };
+
+pub struct Resource<'l, W> where W: 'l {
+    storages: HashMap<String, Rc<Box<Storage>>>,
+    plaindata: RefCell<PlainDataManager<'l>>,
+    texture: RefCell<TextureManager<'l, W>>
+}
+
+impl<'l, W> Resource<'l, W> {
+
+    pub fn new(storages:Vec<Box<Storage>>, pl: &'l PlainDataLoader, tc: &'l TextureCreator<W>) -> Self {
+        Self {
+            storages: Self::convert_to_storage_map(storages),
+            plaindata: RefCell::new(PlainDataManager::new(pl)),
+            texture: RefCell::new(TextureManager::new(tc))
+        }
+    }
+
+    pub fn load_plaindata(&self, storage_name: &str, path: &str) -> Result<Rc<Vec<u8>>, String> {
+        self.plaindata.borrow_mut().load(try!(self.get_storage(storage_name)), path)
+    }
+
+    pub fn load_texture(&self, storage_name: &str, path: &str) -> Result<Rc<Texture<'l>>, String> {
+        self.texture.borrow_mut().load(try!(self.get_storage(storage_name)), path)
+    }
+
+    pub fn list(&self, storage_name: &str, dir: Option<&str>) -> Result<Vec<String>, String> {
+        try!(self.get_storage(storage_name)).list(dir)
+    }
+
+    pub fn save(&self, storage_name: &str, path: &str, data: &Vec<u8>) -> Result<(), String> {
+        try!(self.get_storage(storage_name)).save(path, data)
+    }
+
+    fn convert_to_storage_map(storages: Vec<Box<Storage>>) -> HashMap<String, Rc<Box<Storage>>> {
+        let mut storage_map: HashMap<String, Rc<Box<Storage>>>  = HashMap::new();
+        for storage in storages {
+            storage_map.insert(storage.name(), Rc::new(storage));
+        }
+        storage_map
+    }
+
+    fn get_storage(&self, storage_name: &str) -> Result<Rc<Box<Storage>>, String> {
+        let storage = try!(self.storages.get(storage_name).ok_or(format!("storage not found: {}", storage_name))).clone();
+        Ok(storage)
+    }
+
+
+}
+
 pub mod storage;
 pub mod manager;
