fb1f032588ad626b063e679ffe90c8f08a1f8e2b
Imota Eru <threnody96@gmail.com>
2018-06-12 09:33:22 +0900

    master

--- src/canvas/mod.rs
+++ src/canvas/mod.rs
@@ -2,28 +2,32 @@ extern crate sdl2;
 
 use std::cell::RefCell;
 use self::sdl2::rect::{Rect, Point};
-use self::sdl2::render::{ Canvas, Texture };
+use self::sdl2::render::{ Canvas, Texture, TextureCreator };
 use self::sdl2::video::Window;
 use std::cmp::{max, min};
 
 #[derive(Clone)]
-pub struct VirtualCanvas<'a> {
-    canvas: &'a RefCell<Canvas<Window>>,
+pub struct VirtualCanvas<'l, T> where T: 'l {
+    canvas: &'l RefCell<Canvas<Window>>,
+    texture_creator: &'l TextureCreator<T>,
     rect: Option<Rect>
 }
 
-impl<'a> VirtualCanvas<'a> {
+impl<'l, T> VirtualCanvas<'l, T> where T: 'l {
 
-    pub fn new(canvas: &'a RefCell<Canvas<Window>>) -> Self {
+    pub fn new(canvas: &'l RefCell<Canvas<Window>>, texture_creator: &'l TextureCreator<T>) -> Self {
         let (w, h) = canvas.borrow().window().size();
-        Self { canvas: canvas, rect: Some(Rect::new(0, 0, w, h)) }
+        Self { canvas: canvas, texture_creator: texture_creator, rect: Some(Rect::new(0, 0, w, h)) }
     }
 
-    pub fn sub_canvas(&self, rect: Rect) -> Self {
-        if self.rect.is_none() { return Self { canvas: self.canvas, rect: None }; }
-        let cr = self.rect.unwrap();
-        let nr = Rect::new(cr.x() + rect.x(), cr.y() + rect.y(), rect.width(), rect.height());
-        Self { canvas: self.canvas, rect: Self::overlap_rect(&cr, &nr) }
+    pub fn sub_canvas(&self, rect: Rect, f: Box<Fn(VirtualCanvas<'l, T>) + 'l>) {
+        if self.rect.is_none() { 
+            f(Self { rect: None, .. *self });
+        } else {
+            let cr = self.rect.unwrap();
+            let nr = Rect::new(cr.x() + rect.x(), cr.y() + rect.y(), rect.width(), rect.height());
+            f(Self { rect: Self::overlap_rect(&cr, &nr), .. *self });
+        }
     }
 
     pub fn tcopy(&self, t: &Texture, p: Point, clip: Option<Rect>) -> Result<(), String> {
@@ -31,7 +35,7 @@ impl<'a> VirtualCanvas<'a> {
         let tq = t.query();
         let canvas_rect = self.rect.unwrap();
         let real_clip = clip.unwrap_or(Rect::new(0, 0, tq.width, tq.height));
-        let real_point = Point::new(p.x() + canvas_rect.x(), p.y() + canvas_rect.y());
+        let real_point = Point::new(p.x() + canvas_rect.x() - (real_clip.width() as i32) / 2, p.y() + canvas_rect.y() - (real_clip.height() as i32) / 2);
         let mut c = self.canvas.borrow_mut();
         c.set_viewport(canvas_rect);
         c.copy(t, real_clip, Some(Rect::new(real_point.x(), real_point.y(), real_clip.width(), real_clip.height())))
