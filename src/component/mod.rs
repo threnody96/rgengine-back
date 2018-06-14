46199fa56765079709e2c98204f36903694c9b19
Imota Eru <threnody96@gmail.com>
2018-06-15 01:36:12 +0900

    master

--- src/component/mod.rs
+++ src/component/mod.rs
@@ -1,37 +1,59 @@
 use std::cell::RefCell;
-use ::sdl2::rect::Rect;
+use std::rc::Rc;
+use ::sdl2::rect::{ Point, Rect };
+use ::sdl2::render::Texture;
 use self::operation::Operation;
 
+#[derive(Clone,Copy)]
 pub struct ComponentOption {
     position: Rect,
     angle: f64,
     alpha: u8
 }
 
-pub struct Component<P, S> {
+pub struct Component<'l, P, S> {
     option: ComponentOption,
     props: P,
     state: RefCell<S>,
-    operations: RefCell<Vec<Box<Operation>>>
+    operations: RefCell<Vec<Operation<'l>>>
 }
 
-impl<P, S> Component<P, S> {
+pub trait RenderableComponent {
+    fn render(&self);
+}
+
+impl<'l, P, S> Component<'l, P, S> {
     
     pub fn new(option: ComponentOption, props: P, state: S) -> Self {
         Self { option: option, props: props, state: RefCell::new(state), operations: RefCell::new(Vec::new()) }
     }
 
-    pub fn r(&self, operation: Box<Operation>) {
+    fn r(&self, operation: Operation<'l>) {
         self.operations.borrow_mut().push(operation);
     }
 
-    pub fn render(&self) -> Vec<Box<Operation>> {
-        let mut orig_operations = self.operations.borrow_mut();
-        let mut operations: Vec<Box<Operation>> = Vec::new();
-        while orig_operations.len() > 0 {
-            operations.push(orig_operations.remove(0));
+    pub fn execute<CP, CS>(&self, child_component: Component<'l, CP, CS>) where Component<'l, CP, CS>: RenderableComponent {
+        child_component.render();
+        let mut orig_operations = child_component.operations.borrow_mut();
+        let mut operations: Vec<Operation> = Vec::new();
+        while orig_operations.len() > 0 { operations.push(orig_operations.remove(0)); }
+        if operations.len() != 0 {
+            self.r(Operation::Group { option: child_component.option, operations: operations });
         }
-        operations
+    }
+
+    pub fn copy(&self, t: Rc<Texture<'l>>, p: Point, clip: Option<Rect>) {
+        self.r(Operation::Copy { t: t, p: p, clip: clip });
+    }
+
+    pub fn zoom(&self, t: Rc<Texture<'l>>, p: Point, clip: Option<Rect>, zoom_x: Option<f32>, zoom_y: Option<f32>) {
+        self.r(Operation::Zoom {
+            t: t,
+            p: p,
+            clip: clip,
+            zoom_x: zoom_x,
+            zoom_y: zoom_y
+        });
     }
 
 }
--- src/component/operation/mod.rs
+++ src/component/operation/mod.rs
@@ -1,1 +1,15 @@
-pub trait Operation {}
+use std::rc::Rc;
+use super::ComponentOption;
+use ::sdl2::render::Texture;
+use ::sdl2::rect::{ Point, Rect };
+
+pub trait OperationExecuter {
+    fn operation_execute(&self, operation: Operation);
+}
+
+pub enum Operation<'l> {
+    Group { option: ComponentOption, operations: Vec<Operation<'l>> },
+    Copy { t: Rc<Texture<'l>>, p: Point, clip: Option<Rect> },
+    Zoom { t: Rc<Texture<'l>>, p: Point, clip: Option<Rect>, zoom_x: Option<f32>, zoom_y: Option<f32> }
+}
+
