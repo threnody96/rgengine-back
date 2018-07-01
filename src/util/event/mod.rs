4c20d9a0e5feca4b5d58ad0b1ebf24c7b195d439
Imota Eru <threnody96@gmail.com>
2018-07-01 15:04:31 +0900

    master

--- src/util/event/mod.rs
+++ src/util/event/mod.rs
@@ -1,10 +1,15 @@
 use ::sdl2::EventPump;
 use self::types::KeyType;
 
+pub trait InputDataCore {
+
+}
+
 pub struct InputData {
     key: KeyType,
     state: bool,
-    frame: u32
+    frame: u32,
+    core: Box<InputDataCore>
 }
 
 pub mod types;
