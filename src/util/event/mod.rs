4e36c7137d44ce642cf93b417c51777b1e4ee6b2
Imota Eru <threnody96@gmail.com>
2018-07-02 21:38:38 +0900

    master

--- src/util/event/mod.rs
+++ src/util/event/mod.rs
@@ -1,15 +1,29 @@
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::collections::HashMap;
 use ::sdl2::EventPump;
 use self::types::KeyType;
+use self::values::KeyValue;
 
-pub trait InputDataCore {
-
-}
+type KeyMap = HashMap<KeyType, KeyValue>;
 
 pub struct InputData {
-    key: KeyType,
-    state: bool,
-    frame: u32,
-    core: Box<InputDataCore>
+    event: Rc<RefCell<EventPump>>,
+    keyMap: RefCell<KeyMap>
+}
+
+impl InputData {
+
+    pub fn new(event: Rc<RefCell<EventPump>>) -> Self {
+        Self { event: event, keyMap: RefCell::new(KeyMap::new()) }
+    }
+
+    pub fn update(&self) {
+        let mut keymap = KeyMap::new();
+        self.keyMap.replace(keymap);
+    }
+
 }
 
 pub mod types;
+pub mod values;
---
+++ src/util/event/values/mod.rs
@@ -0,0 +1,3 @@
+pub enum KeyValue {
+    Keyboard
+}
